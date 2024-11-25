e6a72e47c0c794ab0e8160b1ef68b61a463ddd88
Maurice Tin <maurice.tin@gli.id>
2024-11-25 08:42:08 +0700

    first commit

---
+++ src/test/java/iOS/FirstiOSTest.java
@@ -0,0 +1,36 @@
+package iOS;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstiOSTest {
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("automationName", "XCUITest");
+        caps.setCapability("platformVersion", "9.0");
+        caps.setCapability("deviceName", "Android Emulator");
+        caps.setCapability("app", System.getProperty("user.dir")+"/apps/ApiDemos.apk");
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),caps);
+    }
+
+    @Test
+    public void click_App_Button(){
+        //code
+    }
+
+    @Test void tearDown(){
+        if (null != driver) {
+            driver.quit();
+        }
+    }
+}
